65879417329b88bbb678518e829926f583184fe3
wonyongpark89 <wypark5121@gmail.com>
2026-07-17 15:23:12 +0100

    ..

--- cpd-temp-new/feedback-config.js
+++ cpd-temp-new/feedback-config.js
@@ -2,4 +2,4 @@
   Paste the deployed Google Apps Script web-app URL between the quotes below.
   Example: https://script.google.com/macros/s/ABC123/exec
 */
-window.GRENFELL_CPD_FEEDBACK_ENDPOINT = '';
+window.GRENFELL_CPD_FEEDBACK_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzY6hey29LhkLHXPiCFdJXynzd-pbbJ3GcpobnP8w-PDeaSbFuyGEsv13ckoS8yAw27uw/exec';
